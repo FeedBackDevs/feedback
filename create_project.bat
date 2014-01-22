78f40291623c12adb7682783ee3e8dd8beff53dc
Manu Evans <turkeyman@gmail.com>
2014-01-22 12:22:06 +1000

    Updated create_project.bat to support VS2012 projects.

--- create_project.bat
+++ create_project.bat
@@ -1,15 +1,17 @@
 @ECHO OFF
 
 ECHO Select the type of project you would like to create:
-ECHO 1. Visual Studio 2010 Solution
-ECHO 2. Visual Studio 2008 Solution
-ECHO 3. Visual Studio 2005 Solution
-ECHO 4. Visual Studio 2003 Solution
-ECHO 5. Visual Studio 2002 Solution
-ECHO 6. GNU Makefile
+ECHO 1. Visual Studio 2012 Solution
+ECHO 2. Visual Studio 2010 Solution
+ECHO 3. Visual Studio 2008 Solution
+ECHO 4. Visual Studio 2005 Solution
+ECHO 5. Visual Studio 2003 Solution
+ECHO 6. Visual Studio 2002 Solution
+ECHO 7. GNU Makefile
 
-CHOICE /N /C:12345678 /M "[1-6]:"
+CHOICE /N /C:1234567 /M "[1-7]:"
 
+IF ERRORLEVEL ==7 GOTO SEVEN
 IF ERRORLEVEL ==6 GOTO SIX
 IF ERRORLEVEL ==5 GOTO FIVE
 IF ERRORLEVEL ==4 GOTO FOUR
@@ -18,29 +20,33 @@ IF ERRORLEVEL ==2 GOTO TWO
 IF ERRORLEVEL ==1 GOTO ONE
 GOTO END
 
-:SIX
+:SEVEN
  ECHO Creating GNU Makefile...
  bin\premake5.exe gmake
  GOTO END
-:FIVE
+:SIX
  ECHO Creating VS2002 Project...
  bin\premake5.exe vs2002
  GOTO END
-:FOUR
+:FIVE
  ECHO Creating VS2003 Project...
  bin\premake5.exe vs2003
  GOTO END
-:THREE
+:FOUR
  ECHO Creating VS2005 Project...
  bin\premake5.exe vs2005
  GOTO END
-:TWO
+:THREE
  ECHO Creating VS2008 Project...
  bin\premake5.exe vs2008
  GOTO END
-:ONE
+:TWO
  ECHO Creating VS2010 Project...
  bin\premake5.exe vs2010
+ GOTO END
+:ONE
+ ECHO Creating VS2012 Project...
+ bin\premake5.exe vs2012
  GOTO END
 
 :END
